519677b0accefacdfb57122f842b93df4f419d46
dilettasantovito <80915476+dilettasantovito@users.noreply.github.com>
2023-10-05 11:35:16 +0200

    Update 02.populations.R

--- Code/02.populations.R
+++ Code/02.populations.R
@@ -1,5 +1,6 @@
 # uinstall.pcakages("spatstat")
 library(spatstat)
+library(terra)
 
 bei 
 plot(bei) 
@@ -17,3 +18,21 @@ density_map <- density(bei)
 par(mfrow=c(2,1))
 plot(bei.extra[[1]])
 plot(density_map)
+
+bei.rast <- rast(bei.extra[[1]])
+density.rast <- rast(density_map)
+
+randompoints <- spatSample(bei.rast, 100, "random", as.points = TRUE)
+par(mfrow=c(2,1))
+plot(bei.rast)
+points(randompoints)
+plot(density.rast)
+points(randompoints)
+
+# bei.rast and density.rast do not have the same extent and do not have the same
+# number of rows/columns, so I can't stack them
+
+bei.points <- terra::extract(bei.rast, randompoints)
+density.points <- terra::extract(density.rast, randompoints)
+
+pointmaps <- data.frame(bei.points[1:2], density.points[2])
